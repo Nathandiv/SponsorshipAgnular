fee71ae2fbe2fa1b57266db814b9eed1fd06949f
Nathandiv <k.n.hlungwani@gmail.com>
2024-09-02 09:14:26 +0200

    added footer on about us page

--- src/app/pages/about-us/about-us.component.ts
+++ src/app/pages/about-us/about-us.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { NavComponent } from '../../UI/Shared-UI/nav/nav.component';
+import { FooterComponent } from '../../UI/Shared-UI/footer/footer.component';
 
 @Component({
   selector: 'app-about-us',
   standalone: true,
-  imports: [NavComponent],
+  imports: [NavComponent,FooterComponent],
   templateUrl: './about-us.component.html',
   styleUrl: './about-us.component.css'
 })
